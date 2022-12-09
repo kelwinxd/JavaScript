155fc1df824f5f19a3f9b89c7673909e3825dbe9
kelwinxd <kelwin_esechiel@hotmail.com>
2022-12-09 20:21:07 -0300

    a

---
+++ ElementSelector/script.js
@@ -0,0 +1,16 @@
+ const body = document.querySelector('body')
+ const but = document.querySelector('button')
+ const hd = document.querySelector('header')
+ const ul = document.querySelector('ul')
+ const ula = document.getElementById('li')
+ 
+ function change(){
+    body.classList.add('cel')
+     hd.classList.add('hd')
+     ul.classList.add('ul')
+     ula.classList.add('ula')
+   
+ }
+
+
+
